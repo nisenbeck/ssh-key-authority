46bc7b66f8cf3f0413a87fe569687469d085f598
Nico Isenbeck <nico.isenbeck@web.de>
2025-11-20 16:21:11 +0100

    Allow users to configure number of events shown in activity view

--- templates/activity.php
+++ templates/activity.php
@@ -1,4 +1,18 @@
-<h1>Activity</h1>
+<div class="clearfix">
+	<h1 class="pull-left">Activity</h1>
+	<form class="form-inline pull-right">
+		<div class="form-group">
+			<label for="event-limit">Show:</label>
+			<select id="event-limit" class="form-control input-sm" onchange="window.location.href='?limit=' + this.value">
+				<option value="50" <?php if($this->get('limit') == 50) echo 'selected'; ?>>50</option>
+				<option value="100" <?php if($this->get('limit') == 100) echo 'selected'; ?>>100</option>
+				<option value="200" <?php if($this->get('limit') == 200) echo 'selected'; ?>>200</option>
+				<option value="500" <?php if($this->get('limit') == 500) echo 'selected'; ?>>500</option>
+				<option value="1000" <?php if($this->get('limit') == 1000) echo 'selected'; ?>>1000</option>
+			</select>
+		</div>
+	</form>
+</div>
 <table class="table table-searchable">
 	<col>
 	<col>
--- views/activity.php
+++ views/activity.php
@@ -4,11 +4,20 @@ if(!$active_user->admin && count($active_user->list_admined_servers()) == 0 && c
 	die;
 }
 
+// Get limit from URL parameter, default to 100
+$limit = isset($_GET['limit']) ? intval($_GET['limit']) : 100;
+// Validate limit (must be one of the allowed values)
+$allowed_limits = array(50, 100, 200, 500, 1000);
+if (!in_array($limit, $allowed_limits)) {
+	$limit = 100;
+}
+
 $content = new PageSection('activity');
+$content->set('limit', $limit);
 if($active_user->admin) {
-	$content->set('events', $event_dir->list_events());
+	$content->set('events', $event_dir->list_events(array(), array(), $limit));
 } else {
-	$content->set('events', $active_user->list_events());
+	$content->set('events', $active_user->list_events(array(), array(), $limit));
 }
 
 $page = new PageSection('base');
