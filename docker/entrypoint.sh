44df79f8af1552dcafa2ba2e7572f3763f0aa599
Nico Isenbeck <nico.isenbeck@web.de>
2025-11-26 09:53:24 +0100

    Updated Docker setup / Docker Compose config added

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,30 +1,30 @@
-#!/usr/bin/env ash
-if [ `whoami` == 'keys-sync' ]; then
+#!/usr/bin/env bash
+if [ "$(whoami)" = 'keys-sync' ]; then
   if [ ! -r /ska/config/config.ini ]; then
       echo "config.ini not found or incorrect permissions."
       echo "Permissions must be $(id -u keys-sync):$(id -g keys-sync) with at least 400"
       exit 1
   fi
   if [ ! -r /ska/config/keys-sync ]; then
-      echo "private key not found or incorrect permissions."
+      echo "Private key not found or incorrect permissions."
+      echo "Create it with: ssh-keygen -t rsa -b 4096 -f ./config/keys-sync -N '' -C 'keys-sync'"
       echo "Permissions must be $(id -u keys-sync):$(id -g keys-sync) with 400"
       exit 1
   fi
   if [ ! -r /ska/config/keys-sync.pub ]; then
-      echo "public key not found or incorrect permissions."
+      echo "Public key not found or incorrect permissions."
       echo "Permissions must be $(id -u keys-sync):$(id -g keys-sync) with at least 400"
       exit 1
   fi
   if ! grep "^timeout_util = GNU coreutils$" /ska/config/config.ini > /dev/null; then
-      echo "timeout_util must be set to GNU coreutils."
+      echo "Timeout_util must be set to GNU coreutils."
       echo "Change it to: timeout_util = GNU coreutils"
       exit 1
   fi
-elif [ $(id -u) = 0 ]; then
-  if ! sudo -u keys-sync /entrypoint.sh; then
+elif [ "$(id -u)" -eq 0 ]; then
+  if ! su - keys-sync -c /entrypoint.sh; then
     exit 1
   fi
-  rsync -a --delete /ska/public_html/ /public_html/
   echo "Waiting for database..."
   for i in $(seq 1 10); do 
     if /ska/scripts/apply_migrations.php; then
@@ -35,9 +35,9 @@ elif [ $(id -u) = 0 ]; then
     sleep 1
   done
   
-  /usr/sbin/crond
+  /usr/sbin/cron
   /ska/scripts/syncd.php --user keys-sync
-  /usr/sbin/php-fpm7 -F
+  apachectl -D FOREGROUND
 else
   echo "Must be executed with root"
 fi
--- docker/healthcheck.sh
+++ docker/healthcheck.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env ash
-for PID_FILE in /var/run/crond.pid /var/run/keys/keys-sync.pid /var/run/php-fpm.pid; do
-	PID=$(cat ${PID_FILE})
-    if ! [ -n "${PID}" -a -d "/proc/${PID}" ]; then
+#!/usr/bin/env bash
+for PID_FILE in /var/run/crond.pid /var/run/keys/keys-sync.pid /var/run/apache2/apache2.pid; do
+    PID=$(cat "${PID_FILE}" 2>/dev/null || true)
+    if ! [ -n "${PID}" ] || ! [ -d "/proc/${PID}" ]; then
         exit 1
     fi
 done
